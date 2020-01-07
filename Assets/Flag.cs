c385f68f9b3b2f4fefa2c26328bfe1f9ec18bd3e
Tod Schlegelmilch <tschlegelmilch@pillartechnology.com>
2020-01-07 11:31:52 -0500

    Add assets

---
+++ Assets/Flag.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject player = other.gameObject;
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(-500, 200));
+            Debug.Log("GREEN");
+        }
+    }
+}
