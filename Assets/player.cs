7131d66b647a80c5ffa50239ed4433233397f5e1
Paul <paul.l.glinski@accenture.com>
2020-01-07 10:17:54 -0500

    Level 0 spaceguy moves right

---
+++ Assets/player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float maxSpeed;
+    private Rigidbody2D rigidbody2D;
+    Vector2 accel;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        accel = new Vector2(10, 0);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+       
+        if(rigidbody2D.velocity.magnitude < maxSpeed)
+            rigidbody2D.AddForce(accel);
+    }
+}
