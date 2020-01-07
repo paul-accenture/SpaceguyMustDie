f3d71f13179d90d28bc5755d7c94468856f4454c
Tod Schlegelmilch <tschlegelmilch@pillartechnology.com>
2020-01-07 14:59:02 -0500

    Added Assets

---
+++ Assets/Grid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Grid : MonoBehaviour
+{
+    GridLayout gridLayout;
+    public GameObject hiliteSprite;
+    public Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gridLayout = transform.parent.GetComponentInParent<GridLayout>();
+        gridLayout = GetComponentInParent<GridLayout>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int coordinate = gridLayout.WorldToCell(mouseWorldPos);
+            hiliteSprite.transform.SetPositionAndRotation(gridLayout.CellToWorld(coordinate) + offset, new Quaternion()) ;
+
+        }
+
+        if(Input.GetKeyUp(KeyCode.J))
+        {
+            Instantiate(Resources.Load("jumpBoard"), hiliteSprite.transform.position + new Vector3( 0, (float).7, 0), hiliteSprite.transform.rotation);
+        }
+    }
+}
---
+++ Assets/jumpPad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpPad : MonoBehaviour
+{
+    public player player;
+    public Vector2 jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("C'MON JUMP");
+            player.GetRigidbody2D().AddForce(jump);
+        }
+    }
+}
