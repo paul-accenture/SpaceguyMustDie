48af25071e9fabf3a2836e40f53ea36b69b5488f
Paul <paul.l.glinski@accenture.com>
2020-01-17 10:19:49 -0500

    Update tutorial complete text

--- Assets/Scripts/SwitchSceneTextChange.cs
+++ Assets/Scripts/SwitchSceneTextChange.cs
@@ -13,7 +13,7 @@ public class SwitchSceneTextChange : MonoBehaviour
         switchSceneText = GameObject.FindGameObjectWithTag("SwitchSceneText").GetComponent<Text>();
 
         if (idManager.id < 1)
-            switchSceneText.text = "THE TUTORIAL IS OVER DETERMINE WHO WILL GO FIRST AND PASS THE COMPUTER TO THEM.\n\nPRESS [SPACE] TO CONTINUE.";
+            switchSceneText.text = "CONGRATULATIONS! You have completed the tutorial! Please select who will go first and pass the computer to them.\n\nPRESS [SPACE] TO CONTINUE.";
         else
             switchSceneText.text = "PASS THE COMPUTER TO THE OTHER PLAYER.\n\nPRESS [SPACE] TO CONTINUE.";
     }
