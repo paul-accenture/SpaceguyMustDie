22e58206acf4351f7ee3c7137b7600621ff7e3cb
Tod Schlegelmilch <tschlegelmilch@pillartechnology.com>
2020-01-27 15:15:54 -0500

    Updated interstitial text

--- Assets/Scripts/SwitchSceneTextChange.cs
+++ Assets/Scripts/SwitchSceneTextChange.cs
@@ -13,9 +13,9 @@ public class SwitchSceneTextChange : MonoBehaviour
         switchSceneText = GameObject.FindGameObjectWithTag("SwitchSceneText").GetComponent<Text>();
 
         if (idManager.id < 1)
-            switchSceneText.text = "CONGRATULATIONS! You have completed the tutorial! Please select who will go first and pass the computer to them.\n\nPRESS [SPACE] TO CONTINUE.";
+            switchSceneText.text = "CONGRATULATIONS! You have completed the tutorial! PLAYER ONE, GET READY! (No peeking, Player Two!)\n\nPRESS [SPACE] TO CONTINUE.";
         else
-            switchSceneText.text = "PASS THE COMPUTER TO THE OTHER PLAYER.\n\nPRESS [SPACE] TO CONTINUE.";
+            switchSceneText.text = "NEXT LEVEL LOADED!\nPLAYER ONE, GET READY!\n\nPRESS [SPACE] TO CONTINUE.";
     }
 
     // Update is called once per frame
